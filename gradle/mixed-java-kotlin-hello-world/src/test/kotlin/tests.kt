788621ef1ac1f87c0e2cb41f3ad2be67b265a782
Leonid Shalupov <shalupov@diverse.org.ru>
2013-03-21 19:12:24 +0400

    gradle demo project

---
+++ gradle/mixed-java-kotlin-hello-world/src/test/kotlin/tests.kt
@@ -0,0 +1,19 @@
+package demo 
+
+import com.google.common.primitives.Ints
+import com.google.common.base.Joiner
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource() {
+    test fun f() {
+        val example : KotlinGreetingJoiner = KotlinGreetingJoiner(Greeter("Hi"))
+        example.addName("Harry")
+        example.addName("Ron")
+        example.addName(null)
+        example.addName("Hermione")
+
+        assertEquals(example.getJoinedGreeting(), "Hi Harry and Ron and Hermione")
+    }
+}
+
