bc4303752f8e05af7a2d64852fcf61b69746c9ee
Leonid Shalupov <shalupov@diverse.org.ru>
2013-04-01 16:02:02 +0400

    gradle hello-world example

---
+++ gradle/hello-world/src/main/kotlin/helloWorld.kt
@@ -0,0 +1,16 @@
+package demo
+
+import com.google.common.base.Joiner
+import java.util.ArrayList
+
+fun getGreeting() : String {
+    val words = ArrayList<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return Joiner.on(" ").join(words)
+}
+
+fun main(args : Array<String>) {
+    println(getGreeting())
+}
---
+++ gradle/hello-world/src/test/kotlin/tests.kt
@@ -0,0 +1,11 @@
+package demo 
+
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource() {
+    test fun f() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
+
