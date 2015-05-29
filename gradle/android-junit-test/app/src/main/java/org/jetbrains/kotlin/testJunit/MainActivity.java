910b8e1c7911318774c9e309ba26c67803504df9
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2015-05-29 14:21:29 +0300

    Initial commit for android-junit-example

---
+++ gradle/android-junit-test/app/src/main/java/org/jetbrains/kotlin/testJunit/MainActivity.java
@@ -0,0 +1,20 @@
+package org.jetbrains.kotlin.testJunit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ gradle/android-junit-test/app/src/test/kotlin/org/jetbrains/kotlin/testJunit/test/KotlinJunitTest.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.kotlin.testJunit.test
+
+import org.junit.Assert
+import org.junit.Test
+
+
+public class KotlinJunitTest {
+    Test public fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+}
+
