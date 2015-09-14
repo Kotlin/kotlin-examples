d0e6889d15a98d2a1ed0d399c65d1e8b7b5f6f6e
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2015-09-14 16:34:00 +0200

    lambda

--- gradle/android-mixed-java-kotlin-project/AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/MainActivity2.kt
+++ gradle/android-mixed-java-kotlin-project/AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/MainActivity2.kt
@@ -15,17 +15,20 @@ open class MainActivity2 : Activity() {
         setContentView(R.layout.activity_main2)
 
         var next: Button = findViewById(R.id.Button02) as Button
-        next.setOnClickListener(object : View.OnClickListener {
-            public override fun onClick(view: View) {
-                val intent: Intent = Intent()
-                setResult(Activity.RESULT_OK, intent)
-                finish()
-            }
-        })
+
+//        next.setOnClickListener(object : View.OnClickListener {
+//            public override fun onClick(view: View) {
+//                val intent: Intent = Intent()
+//                setResult(Activity.RESULT_OK, intent)
+//                finish()
+//            }
+//        })
+
+        next.setOnClickListener {  finish()  }
+
     }
 
     public override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_activity2, menu)
         return true
     }
