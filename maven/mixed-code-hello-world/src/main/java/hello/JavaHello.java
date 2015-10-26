03959f2351de1afa23f7e7d920fb2666082f13f4
Dmitry Jemerov <yole@jetbrains.com>
2015-10-26 14:13:43 +0100

    use new package facades

--- maven/mixed-code-hello-world/src/main/java/hello/JavaHello.java
+++ maven/mixed-code-hello-world/src/main/java/hello/JavaHello.java
@@ -4,11 +4,11 @@ public class JavaHello {
   public static String JavaHelloString = "Hello from Java!";
 
   public static String getHelloStringFromKotlin() {
-    return HelloPackage.getKotlinHelloString();
+    return KotlinHelloKt.getKotlinHelloString();
   }
 
   public static void main(String[] args) {
     System.out.println(getHelloStringFromKotlin());
-    System.out.println(HelloPackage.getHelloStringFromJava());
+    System.out.println(KotlinHelloKt.getHelloStringFromJava());
   }
 }
