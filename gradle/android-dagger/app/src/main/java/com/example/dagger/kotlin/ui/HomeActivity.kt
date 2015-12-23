6afc0dbcb9512fbee1af0685ad056ed51b7991f7
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-12-23 18:30:26 +0300

    Update android-dagger example

--- gradle/android-dagger/app/src/main/java/com/example/dagger/kotlin/ui/HomeActivity.kt
+++ gradle/android-dagger/app/src/main/java/com/example/dagger/kotlin/ui/HomeActivity.kt
@@ -20,7 +20,7 @@ import android.os.Bundle
 import com.example.dagger.kotlin.DemoActivity
 import com.example.dagger.kotlin.DemoApplication
 import com.example.dagger.kotlin.R
-import kotlinx.android.synthetic.activity_main.locationInfo
+import kotlinx.android.synthetic.main.activity_main.locationInfo
 import javax.inject.Inject
 
 public class HomeActivity : DemoActivity() {
