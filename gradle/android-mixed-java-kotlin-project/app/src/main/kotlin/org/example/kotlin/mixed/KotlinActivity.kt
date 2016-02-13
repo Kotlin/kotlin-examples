38d586facafd33dc05e1127639f26d5d0026be20
Ivan Morgillo <imorgillo@gmail.com>
2016-02-13 11:06:57 +0100

    Add a Java Activity with a button to start a Kotlin activity

---
+++ gradle/android-mixed-java-kotlin-project/app/src/main/kotlin/org/example/kotlin/mixed/KotlinActivity.kt
@@ -0,0 +1,12 @@
+package org.example.kotlin.mixed
+
+import android.app.Activity
+import android.os.Bundle
+
+class KotlinActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
