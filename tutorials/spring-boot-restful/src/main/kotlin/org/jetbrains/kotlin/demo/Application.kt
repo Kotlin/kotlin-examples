872364caaba9e3e0255482c3adf12335a2df2ee4
syeutyu <syeuty@naver.com>
2018-01-31 16:26:31 +0900

    Intellij Idea to spring_boot_restful compile

--- tutorials/spring-boot-restful/src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
+++ tutorials/spring-boot-restful/src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application
+open class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
