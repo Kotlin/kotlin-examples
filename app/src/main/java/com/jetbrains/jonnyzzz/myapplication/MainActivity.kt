337d47f4b2aabab47ca5ffd63aab55f39667c408
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-09-24 22:59:34 +0200

    property syntax

--- app/src/main/java/com/jetbrains/jonnyzzz/myapplication/MainActivity.kt
+++ app/src/main/java/com/jetbrains/jonnyzzz/myapplication/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    findViewById<TextView>(R.id.main_text).setText(createApplicationScreenMessage())
+    findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
   }
 }
