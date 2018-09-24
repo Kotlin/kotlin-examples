fb956e6ab780788b2e6f6aacdf288433a872ced4
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-09-24 18:19:27 +0200

    android app added

---
+++ app/src/main/java/com/jetbrains/jonnyzzz/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.jonnyzzz.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
