c00594fd715b398b3d71a66e0335aaf29fbd23b6
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-09-26 17:50:48 +0200

    make it generate frameworks

---
+++ common/src/iosMain/kotlin/expect.kt
@@ -0,0 +1,9 @@
+package com.jetbrains.jonnyzzz.common
+
+
+//https://stackoverflow.com/questions/11072804/how-do-i-determine-the-os-version-at-runtime-in-os-x-or-ios-without-using-gesta
+
+
+actual fun platformName(): String {
+  return "iOS"
+}
