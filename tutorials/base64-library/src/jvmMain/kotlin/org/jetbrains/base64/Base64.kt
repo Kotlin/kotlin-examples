16b050c0bae08c15cec10493c7c3ad3b73015cda
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2018-10-04 00:12:19 +0200

    Multiplatform Base64 library

---
+++ tutorials/base64-library/src/jvmMain/kotlin/org/jetbrains/base64/Base64.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.base64
+
+import java.util.*
+
+actual object Base64Factory {
+    actual fun createEncoder(): Base64Encoder = JvmBase64Encoder
+}
+
+object JvmBase64Encoder : Base64Encoder {
+    override fun encode(src: ByteArray): ByteArray = Base64.getEncoder().encode(src)
+    override fun encodeToString(src: ByteArray): String = Base64.getEncoder().encodeToString(src)
+}
---
+++ tutorials/base64-library/src/macosMain/kotlin/base64/Base64.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.base64
+
+private val BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+private val BASE64_MASK: Byte = 0x3f
+private val BASE64_PAD = '='
+
+private val BASE64_INVERSE_ALPHABET = IntArray(256) {
+    BASE64_ALPHABET.indexOf(it.toChar())
+}
+
+private fun Int.toBase64(): Char = BASE64_ALPHABET[this]
+
+actual object Base64Factory {
+    actual fun createEncoder(): Base64Encoder = NativeBase64Encoder
+}
+
+object NativeBase64Encoder : Base64Encoder {
+    override fun encode(src: ByteArray): ByteArray {
+        fun ByteArray.getOrZero(index: Int): Int = if (index >= size) 0 else get(index).toInt()
+
+        val result = ArrayList<Byte>(4 * src.size / 3)
+        var index = 0
+        while (index < src.size) {
+            val symbolsLeft = src.size - index
+            val padSize = if (symbolsLeft >= 3) 0 else (3 - symbolsLeft) * 8 / 6
+            val chunk = (src.getOrZero(index) shl 16) or (src.getOrZero(index + 1) shl 8) or src.getOrZero(index + 2)
+            index += 3
+
+            for (i in 3 downTo padSize) {
+                val char = (chunk shr (6 * i)) and BASE64_MASK.toInt()
+                result.add(char.toBase64().toByte())
+            }
+
+            repeat(padSize) { result.add(BASE64_PAD.toByte()) }
+        }
+
+        return result.toByteArray()
+    }
+}
