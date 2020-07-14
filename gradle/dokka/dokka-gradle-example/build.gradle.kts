d28c269db3c2ba6eef3cfbaba47602df292f8eb7
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-14 11:02:30 +0200

    Move examples into `dokka` subfolder and add kotlin-as-java example

--- gradle/dokka/dokka-gradle-example/build.gradle.kts
+++ gradle/dokka/dokka-gradle-example/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    kotlin("jvm") version "1.4.0-M3"
-    id("org.jetbrains.dokka") version ("1.4.0-M3-dev-65")
+    kotlin("jvm") version "1.4-M3"
+    id("org.jetbrains.dokka") version ("1.4.0-M3-dev-69")
 }
 
 repositories {
@@ -18,12 +18,13 @@ dependencies {
 
 tasks.withType<DokkaTask> {
     dokkaSourceSets {
-        configureEach {
+        register("main") {
             moduleDisplayName = "Dokka Gradle Example"
             includes = listOf("Module.md")
             sourceLink {
                 path = "src/main/kotlin"
-                url = "https://github.com/Kotlin/kotlin-examples/tree/master/gradle/dokka-gradle-example/src/main/kotlin"
+                url = "https://github.com/Kotlin/kotlin-examples/tree/master/" +
+                        "gradle/dokka/dokka-gradle-example/src/main/kotlin"
                 lineSuffix = "#L"
             }
         }
