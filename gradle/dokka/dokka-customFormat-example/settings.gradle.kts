532b3e48abe2fb166e56e8388c17a24d62b9f9d6
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-14 11:06:17 +0200

    Add dokka-customFormat-example

---
+++ gradle/dokka/dokka-customFormat-example/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        jcenter()
+        mavenCentral()
+        maven("https://dl.bintray.com/kotlin/kotlin-dev")
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+
+rootProject.name = "dokka-customFormat-example"
+
